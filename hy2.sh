d4046088ceeeea06d86b65eea003332106af6153
dgz1234 <g671106001@gmail.com>
2025-08-01 08:54:20 +0800

    Update hy2.sh

--- hy2.sh
+++ hy2.sh
@@ -83,7 +83,7 @@ wget_with_retry() {
 # 示例版本(注释掉)
 # HYSTERIA_VERSION="v2.6.1"
 HYSTERIA_VERSION="${HYSTERIA_VERSION:-v2.6.1}"
-wget_with_retry "https://github.com/dgz1234/wxy/raw/main/hysteria-linux-amd64-${HYSTERIA_VERSION}" /usr/local/bin/hysteria
+wget_with_retry "https://github.com/dgz1234/ap-h2/raw/main/hysteria-linux-amd64-${HYSTERIA_VERSION}" /usr/local/bin/hysteria
 chmod +x /usr/local/bin/hysteria
 
 mkdir -p /etc/hysteria/
